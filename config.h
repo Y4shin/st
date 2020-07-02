6f5dffe4a136c4e5090cc7c37e2e528e87d63c5f
Patric Plattner <patric@plattner.com.de>
2020-07-02 02:03:37 +0200

    changing fontsize for low res screens

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "LiberationMono:pixelsize=20:antialias=true:autohint=true";
-static char *font2[] = { "mono:pixelsize=20:antialias=true:autohint=true" };
+static char *font = "LiberationMono:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "mono:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
